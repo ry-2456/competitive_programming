138244c9ada21972a0e7a1c3df3042da01ae3f70
ry-2456 <ittanmomennimakaretyaimashita@gmail.com>
2020-10-01 11:38:20 +0900

    [add] Add prime_factorization.cpp template

---
+++ algorithm/number_theory/prime_factorization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::pair<long long, long long> > prime_factorize(long long N) {
+  std::vector<std::pair<long long, long long> > res;
+
+  return res;
+}
+
+
+int main() {
+  int n; std::cin >> n;
+}
