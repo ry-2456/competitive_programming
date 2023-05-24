dd699191f83134a11144e5656407196a2334ab1b
Rintaro Yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2023-05-24 22:52:57 +0900

    オプション引数取得

--- atcoder/abc/run.sh
+++ atcoder/abc/run.sh
@@ -2,10 +2,25 @@
 while (( $# > 0 ))
 do
   case $1 in
-    -n | --number)
-      echo "option"
-      echo $1
-      exit 0
+    -n | --number | --number=*)
+      if [[ "$1" =~ ^--number= ]]; then
+        NUMBER=$(echo $1 | sed -e 's/^--number=//')
+      elif [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'number' requires an arguments." 1>&2
+      else
+        NUMBER="$2"
+        shift
+      fi
+      ;;
+    -l | --level | --level=*)
+      if [[ "$1" =~ ^--level= ]]; then
+        LEVEL=$(echo $1 | sed -e 's/^--level=//')
+      elif [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "'level' requires an arguments." 1>&2
+      else
+        LEVEL="$2"
+        shift
+      fi
       ;;
   esac
   shift
